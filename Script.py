98f278b3c9ae3bcee844423fba7edcfe0c16a3a6
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-03-19 20:50:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,24 +4,24 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-° 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=@TheFPcreator>FPCREATOR</a>
+° 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=@Professor_creater>Profeser</a>
 ° 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ° 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ° 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ° 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ° 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- FM©CREATOR is a open source project. 
+- MF_MovieBot is a open source project. 
 - Source - പോടാ ഞാൻ തരൂല്ല he. he. he... 😂
 
 <b>DEVS:</b>
-- <a href=@TheFM_move>FM_CREATOR</a>"""
+- <a href=@Professor_creater>PROFESER</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and FP©REATOR will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. FPcreator should have admin privillage.
+1. MF_MovieBot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -36,7 +36,7 @@ class script(object):
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. FPCREATOR supports buttons with any telegram media type.
+2. MF_MovieBot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>MF_MOVE_BOT</b>
 
 <b>NOTE:</b>
-these are the extra features of FPCREATOR
+these are the extra features of MF_MovieBot
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
