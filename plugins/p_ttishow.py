a88c64d440b267dbfbdea6ce546e1895343cd611
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-03-25 07:20:11 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,7 +37,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('☠️ 𝖍𝖊𝖑𝖕', url=f"https://t.me/{temp.U_NAME}?start=help"),
             InlineKeyboardButton('🕹️ ᴜᴩᴅᴀᴛᴇ', url='https://t.me/FPHDMOVE')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
