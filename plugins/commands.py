afe8bb31a6cd61a64ba7bc99cc839b2cbe9f7913
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-03-31 17:00:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,10 +45,10 @@ async def start(client, message):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🕹️ ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
+            InlineKeyboardButton('ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
             ],[
-            InlineKeyboardButton('ʜᴇʟᴩ💤', callback_data='help'),
-            InlineKeyboardButton('☠️ ᴀʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('ʜᴇʟᴩ ᴍᴇ', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -88,10 +88,10 @@ async def start(client, message):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🕹️ ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
+            InlineKeyboardButton('ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
             ],[
-            InlineKeyboardButton('ʜᴇʟᴩ💤', callback_data='help'),
-            InlineKeyboardButton('☠️ᴀʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('ʜᴇʟᴩ', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
